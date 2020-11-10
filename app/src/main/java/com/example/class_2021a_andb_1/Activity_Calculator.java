e46fe3e0f2f3ec9997aaccaca3757b1c712a6485
yarin <you@example.com>
2020-11-10 16:57:45 +0200

    Initial commit

---
+++ app/src/main/java/com/example/class_2021a_andb_1/Activity_Calculator.java
@@ -0,0 +1,14 @@
+package com.example.class_2021a_andb_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_Calculator extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity__calculator);
+    }
+}
---
+++ app/src/main/java/com/example/class_2021a_andb_1/Activity_Main.java
@@ -0,0 +1,57 @@
+package com.example.class_2021a_andb_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class Activity_Main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button1);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView txt1 = findViewById(R.id.textview);
+                txt1.setText("Button has been Pressed");
+                Log.i("Yarin", "Hello");
+                txt1.setTextColor(Color.YELLOW);
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+
+}
